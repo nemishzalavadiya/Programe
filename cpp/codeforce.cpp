0e564b5248f59a10ca6631a35d0e3ba8ecf8f594
nemishzalavadiya <13101999znemish@gmail.com>
2020-06-21 18:10:24 +0530

    codeforces

---
+++ cpp/codeforce.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <fstream>
+#include <queue>
+#include <deque>
+#include <bitset>
+#include <iterator>
+#include <list>
+#include <stack>
+#include <map>
+#include <set>
+#include <functional>
+#include <utility>
+#include <climits>
+#include <cmath>
+#include <cstdio>
+#include <cstring>
+#include <iomanip>
+#include <cassert>
+#define ll long long 
+#define vl vector<ll> 
+#define vvl vector<vl> // v(r,vector<ll>(col,init_value?))
+#define fastio ios_base::sync_with_stdio(false), cin.tie(NULL), cout.tie(NULL)
+#define loop(x, n) for(long long x = 0; x < n; x++)
+#define array1d(arr, NN) loop(ii, NN)cin>>arr[ii];
+#define array2d(arr, NN, MM) loop(ii, NN)loop(jj, MM)cin>>arr[ii][jj]
+#define ini(arr, N) loop(ii, N)scanf("%d", &arr[ii]);
+#define pll pair<ll, ll> 
+#define map map<ll,ll>
+#define umap unordered_map<ll,ll>
+#define set set<ll,ll>
+#define ff first
+#define pb push_back
+#define ss second
+#define str to_string
+using namespace std;
+ 
+int main()
+{   
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        long long int n;
+        cin>>n;
+       
+        cout<<"\n";
+    }
+}
