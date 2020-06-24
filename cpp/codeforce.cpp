ef5dc720e3bf4dac494d8f3451806a79ee409866
nemishzalavadiya <13101999znemish@gmail.com>
2020-06-24 12:48:51 +0530

    foobar

--- cpp/codeforce.cpp
+++ cpp/codeforce.cpp
@@ -27,26 +27,64 @@
 #define array1d(arr, NN) loop(ii, NN)cin>>arr[ii];
 #define array2d(arr, NN, MM) loop(ii, NN)loop(jj, MM)cin>>arr[ii][jj]
 #define ini(arr, N) loop(ii, N)scanf("%d", &arr[ii]);
-#define pll pair<ll, ll> 
-#define map map<ll,ll>
-#define umap unordered_map<ll,ll>
-#define set set<ll,ll>
+#define be(x) (x).begin(),(x).end()
+#define rbe(x) (x).rbegin(),(x).rend()
+#define pl pair<ll, ll> 
+#define mapl map<ll,ll>
+#define umapl unordered_map<ll,ll>
+#define setl set<ll,ll>
 #define ff first
 #define pb push_back
 #define ss second
 #define str to_string
+#define de if(debug)
+#define debug 0
 using namespace std;
- 
+const int mod9 = 998244353;
+const int mod7 = 10000007;
+//setbase - cout << setbase (16)a; cout << 100 << endl; Prints 64
+//setfill -   cout << setfill ('x') << setw (5); cout << 77 <<endl;prints xxx77
+//setprecision - cout << setprecision (14) << f << endl; Prints x.xxxx
+//cout.precision(x)  cout<<fixed<<val;  // prints x digits after decimal in val 
+int power(int a,int b){
+	int res=1;
+	while(b>0){
+		if(b%2){
+			res*=a;
+			res%=mod9;
+		}
+		b/=2;
+		a*=a;
+		a%=mod9;
+	}
+	return res;
+}
+
 int main()
 {   
     int t;
     cin>>t;
     while(t--)
     {
-        long long int n;
-        cin>>n;
-       
-        cout<<"\n";
+		ll n,k,happay=0;
+		cin>>n>>k;
+		vl integer(n);
+		vl fri(k);
+		array1d(integer,n);
+		array1d(fri,k);
+		sort(be(integer));
+		sort(be(fri));
+		for (int i = n - k; i < n; ++i)
+            happay += integer[i];
+        for (int i = 0, j = n - k; i < k; ++i) {
+            j -= fri[i] - 1;
+            if (fri[i] == 1) {
+                happay += integer[n - i - 1];
+            } else {
+                happay += integer[j];
+            }
+        }
+		cout<<happay<<endl;
     }
     return 0;
 }
--- python/test.py
+++ python/test.py
@@ -1,28 +1,14 @@
-# Python program to print all primes smaller than or equal to 
-# n using Sieve of Eratosthenes 
+from fractions import Fraction
+l =[]
+for i in range(10):
+	m =[]
+	for j in range(10):
+		if j==0:
+			m.append(Fraction(i,-1))
+			continue
+		m.append(Fraction(i,j))
+	l.append(m)
+for i in l:
+	for j in i:
+		print(j.numerator,j.denominator)
 
-def SieveOfEratosthenes(n): 
-	
-	# Create a boolean array "prime[0..n]" and initialize 
-	# all entries it as true. A value in prime[i] will 
-	# finally be false if i is Not a prime, else true. 
-	prime = [True for i in range(n+1)] 
-	p = 2
-	while (p * p <= n): 
-		
-		# If prime[p] is not changed, then it is a prime 
-		if (prime[p] == True): 
-			
-			# Update all multiples of p 
-			for i in range(p * p, n+1, p): 
-				prime[i] = False
-		p += 1
-	
-	# Print all prime numbers 
-	for p in range(2, n+1): 
-		if prime[p]: 
-			print(p,",",end="") 
-
-# driver program 
-if __name__=='__main__': 
-	SieveOfEratosthenes(10000) 
