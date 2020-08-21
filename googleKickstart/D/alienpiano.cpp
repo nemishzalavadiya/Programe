a01d7f921951e50a7c94330ebb4be5960565bebc
nemishzalavadiya <13101999znemish@gmail.com>
2020-08-21 11:21:39 +0530

    D2

---
+++ googleKickstart/D/alienpiano.cpp
@@ -0,0 +1,89 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <fstream>
+#include <queue>
+#include <deque>
+#include <bitset>
+#include <iterator>
+#include <list>
+#include <stack>
+#include <map>
+#include <unordered_map>
+#include <set>
+#include <unordered_set>
+#include <functional>
+#include <utility>
+#include <climits>
+#include <cmath>
+#include <cstdio>
+#include <cstring>
+#include <iomanip>
+#include <cassert>
+#include <cstdlib>
+#define ll long long
+#define vl vector<ll>
+#define vvl vector<vl>
+#define fastio ios_base::sync_with_stdio(false), cin.tie(NULL), cout.tie(NULL)
+#define loop(x, n) for (long long x = 0; x < n; x++)
+#define array1d(arr, NN) loop(ii, NN) cin >> arr[ii];
+#define array2d(arr, NN, MM) loop(ii, NN) loop(jj, MM) cin >> arr[ii][jj]
+#define ini(arr, N) loop(ii, N) scanf("%d", &arr[ii]);
+#define be(x) (x).begin(), (x).end()
+#define rbe(x) (x).rbegin(), (x).rend()
+#define pl pair<ll, ll>
+#define mapl map<ll, ll>
+#define endl "\n"
+#define umapl unordered_map<ll, ll>
+#define setl set<ll, ll>
+#define usetl unordered_set<ll, ll>
+#define ff first
+#define pb push_back
+#define ss second
+#define str to_string
+#define de if (debug)
+#define debug 0
+using namespace std;
+const int mod9 = 998244353;
+const int mod7 = 10000007;
+int total;
+void printViolation(vector<ll>& st,ll n,int t){
+    int first=1,last=4,count=0;
+    for(int i=1;i<n;i++){
+        if(st[i]>st[i-1]){
+            first++;
+            last=4;
+        }
+        else if(st[i]<st[i-1]){
+            last--;
+            first=1;
+        }
+        if(first>last){
+            count++;
+            first=1;last=4;
+        }
+    }
+    cout<<"Case #"<<total-t<<": "<<count<<endl;
+}
+
+void solve(int t){
+    ll n;
+    cin>>n;
+    vector<ll> store(n);
+    array1d(store,n);
+    printViolation(store,n,t);
+}
+
+int main()
+{
+    fastio;
+    int t;
+    cin >> t;
+    total=t;
+    while (t--)
+    {
+        solve(t);
+    }
+    return 0;
+}
