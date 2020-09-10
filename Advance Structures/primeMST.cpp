0582cd175bf09529ec33cbaffd11789916077239
nemishzalavadiya <13101999znemish@gmail.com>
2020-09-10 12:38:25 +0530

    PrimMST added

---
+++ Advance Structures/primeMST.cpp
@@ -0,0 +1,107 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <fstream>
+#include <queue>
+#include <deque>
+#include <bitset>
+#include <iterator>
+#include <list>
+#include <stack>
+#include <map>
+#include <unordered_map>
+#include <set>
+#include <unordered_set>
+#include <functional>
+#include <utility>
+#include <climits>
+#include <cmath>
+#include <cstdio>
+#include <cstring>
+#include <iomanip>
+#include <cassert>
+#define ll unsigned long long
+#define vl vector<ll>
+#define vvl vector<vl> 
+#define fastio ios_base::sync_with_stdio(false), cin.tie(NULL), cout.tie(NULL)
+#define loop(x, n) for (long long x = 0; x < n; x++)
+#define array1d(arr, NN) loop(ii, NN) cin >> arr[ii];
+#define array2d(arr, NN, MM) loop(ii, NN) loop(jj, MM) cin >> arr[ii][jj]
+#define ini(arr, N) loop(ii, N) scanf("%d", &arr[ii]);
+#define be(x) (x).begin(), (x).end()
+#define rbe(x) (x).rbegin(), (x).rend()
+#define pl pair<ll, ll>
+#define mapl map<ll, ll>
+#define umapl unordered_map<ll, ll>
+#define setl set<ll, ll>
+#define usetl unordered_set<ll, ll>
+#define ff first
+#define pb push_back
+#define ss second
+#define str to_string
+#define de if (debug)
+#define debug 0
+using namespace std;
+const int mod9 = 998244353;
+const int mod7 = 10000007;
+
+#define V 5 
+ 
+int minKey(int key[], bool mstSet[]) 
+{ 
+    int min = INT_MAX, min_index; 
+ 
+    for (int v = 0; v < V; v++) 
+        if (mstSet[v] == false && key[v] < min) 
+            min = key[v], min_index = v; 
+ 
+    return min_index; 
+} 
+ 
+void printMST(int parent[], int graph[V][V]) 
+{ 
+    cout<<"Edge \tWeight\n"; 
+    for (int i = 1; i < V; i++) 
+        cout<<parent[i]<<" - "<<i<<" \t"<<graph[i][parent[i]]<<" \n"; 
+} 
+ 
+
+void primMST(int graph[V][V]) 
+{ 
+    int parent[V]; 
+     
+    int key[V]; 
+     
+    bool mstSet[V]; 
+ 
+    for (int i = 0; i < V; i++) 
+        key[i] = INT_MAX, mstSet[i] = false; 
+    key[0] = 0; 
+    parent[0] = -1; 
+ 
+    // The MST will have V vertices 
+    for (int count = 0; count < V - 1; count++)
+    { 
+        int u = minKey(key, mstSet); 
+        mstSet[u] = true; 
+        for (int v = 0; v < V; v++) 
+ 
+            if (graph[u][v] && mstSet[v] == false && graph[u][v] < key[v]) 
+                parent[v] = u, key[v] = graph[u][v]; 
+    }  
+    printMST(parent, graph); 
+} 
+ 
+int main() 
+{ 
+    int graph[V][V] = { { 0, 2, 0, 6, 0 }, 
+                        { 2, 0, 3, 8, 5 }, 
+                        { 0, 3, 0, 0, 7 }, 
+                        { 6, 8, 0, 0, 9 }, 
+                        { 0, 5, 7, 9, 0 } }; 
+ 
+    primMST(graph); 
+ 
+    return 0; 
+}
